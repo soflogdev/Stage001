a39285b3d67f496e19738b7827eb9e46e494a623
soflogdev <gbonnet@soflog.com>
2014-07-23 12:12:34 +0200

    Lecture premiere ligne fichierres

--- app/src/main/java/com/soflog/sunny/HistoryActivity.java
+++ app/src/main/java/com/soflog/sunny/HistoryActivity.java
@@ -4,48 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
 import com.soflog.sunny.R;
 
-public class SelectUserActivity extends Activity {
-
-    private String[] namesArray;
-    private String[] codesArray;
-    private TextView mTextView;
+public class HistoryActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_user);
-
-
-        // File res reading
-        namesArray = getResources().getStringArray(R.array.names);
-        codesArray = getResources().getStringArray(R.array.codes);
-
-        mTextView = (TextView) findViewById(R.id.textview_helloworld);
+        setContentView(R.layout.activity_history);
     }
 
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        StringBuilder text;
-
-        text = new StringBuilder();
-        text.append( namesArray[0]);
-        text.append( " - "  );
-        text.append( codesArray[0]);
-
-        mTextView.setText(text.toString());
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.select_user, menu);
+        getMenuInflater().inflate(R.menu.history, menu);
         return true;
     }
 
